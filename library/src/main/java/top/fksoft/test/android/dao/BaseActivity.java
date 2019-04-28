19629fb5f500072c1e214fe6d9190e549f4e34b2
Noob <explodingfkl@gmail.com>
2019-04-28 15:22:31 +0800

    fix bugs.

--- library/src/main/java/top/fksoft/test/android/dao/BaseActivity.java
+++ library/src/main/java/top/fksoft/test/android/dao/BaseActivity.java
@@ -176,5 +176,6 @@ public abstract class BaseActivity extends AppCompatActivity implements View.OnC
         super.onDestroy();
         if (database!=null)
             database.close();
+
     }
 }
